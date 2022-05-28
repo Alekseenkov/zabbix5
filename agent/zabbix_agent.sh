6ef1fd302afc0a0188e0c343cf651984164d4b16
Alekseenkov <kseenkovAle@outlook.com>
2022-05-28 16:14:20 +0300

    agent

---
+++ agent/zabbix_agent.sh
@@ -0,0 +1,12 @@
+sudo apt update
+sudo apt upgrade 
+sudo ufw allow 10050/tcp
+wget https://repo.zabbix.com/zabbix/6.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_6.0-1+ubuntu20.04_all.deb
+sudo dpkg -i zabbix-release_6.0-1+ubuntu20.04_all.deb
+sudo apt update
+sudo apt upgrade
+sudo apt install zabbix-agent zabbix-sender
+sudo cat /home/user/zabbix_agentd.conf > /etc/zabbix/zabbix_agentd.conf
+sudo systemctl start zabbix-agent
+sudo systemctl enable zabbix-agent
+systemctl status zabbix-agent
